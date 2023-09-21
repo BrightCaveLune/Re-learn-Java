d8ed6685c0869d5cfebb729cbcf33348579face3
BrightCaveLune <brightcavelune@gmail.com>
2023-09-21 14:26:51 -0500

    Adding Files from eclipse workspace

---
+++ src/introOne/goGO.java
@@ -0,0 +1,25 @@
+package introOne;
+
+public class goGO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//recall standard print.ln function and how it sends to console.
+		System.out.println("test");
+		
+		//loop practice
+		//initialize variable type int to name loopCall as value 3
+		int loopCall = 3;
+		while (loopCall < 4){
+			loopCall++;
+			System.out.println("loop check");
+			//should print loop check once.
+		}
+		
+		//array initialization (int array) and print value at index 2
+		int[] testName = {1, 2, 3};
+		System.out.println(testName[2]);
+		
+	}
+
+}
