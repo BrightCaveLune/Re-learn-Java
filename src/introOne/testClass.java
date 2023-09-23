959b7a365ef74d18fba5ac3c9574f63a260cb007
June <brightcavelune@gmail.com>
2023-09-22 20:42:31 -0500

    working with java import functions

--- src/introOne/testClass.java
+++ src/introOne/testClass.java
@@ -1,5 +1,13 @@
 package introOne;
+//import random function
+import java.util.Random;
 
 public class testClass {
-
+	public static void main(String[] args) {
+		//create new random instance
+		Random rand = new Random();
+		//create upper bounding variable for random function
+		int upperBound = 2;
+		
+	}
 }
