d7151d36da8496898965bb6b87bab7ef1ea8f95e
Juniper <brightcavelune@gmail.com>
2024-01-18 13:39:12 -0600

    N/A

---
+++ src/introOne/Tutorial.java
@@ -0,0 +1,9 @@
+package introOne;
+
+public class tutorial{
+    public static void main(String[] args){
+        byte byteTest = 100; //The byte data type can store whole numbers from -128 to 127.
+        short shortTest = 20000; //The short data type can store whole numbers from -32768 to 32767.
+        int intTest = 3000000000; //The int data type can store whole numbers from -2147483648 to 2147483647.
+    }
+}
